d7b620cadbc123297de90b9b31b43ea342cbdabc
Brittany <brittany.foster@yahoo.com>
2019-08-25 14:56:10 -0700

    Fixed javascript errors

--- app.js
+++ app.js
@@ -1,54 +1,108 @@
 function onReady() {
-  //
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', event => {
-   event.preventDefault();
-   // get the text
-    let title = newToDoText.value;
-    if(title===null||title===undefined||title===""){
-      return
-    }
-    // create a new li
-   let newLi = document.createElement('li');
-   newLi.classList.add("mdl-list__item");
-  let span=document.createElement("span")
-  span.classList.add("mdl-list__item-primary-content")
-   // create a new input
-   let checkbox = document.createElement('input');
+let id = 0;
+let toDos = [];
+const addToDoForm = document.getElementById('addToDoForm');
 
-   // set the input's type to checkbox
-   checkbox.type = "checkbox";
-   // set the title
-  span.textContent = title;
+/**
+* Delete the list item that the Delete Button is in.
+* @param {HTMLButtonElement} buttonElement
+*/
+function deleteCheckedToDos(buttonElement) {
+let liElement = buttonElement.parentNode.parentNode.parentNode;
+var liId = parseInt(liElement.id);
+toDos = toDos.filter(function(toDo) {
+return !(toDo.todoId === liId
+&& isCheckBoxClicked(buttonElement.parentElement.parentElement.childNodes[1]));
+});
 
-  // attach the checkbox to the li
-  span.appendChild(checkbox)
-  newLi.appendChild(span)
-  // attach the li to the ul
-  toDoList.appendChild(newLi);
-     //empty the input
-   newToDoText.value = '';
- });
+id = id - 1;
+// Once the appropriate elements have been deleted, render the UI
+renderTheUI();
 }
 
+/**
+* Check to see if the input element checkbox is checked.
+* @param {HTMLInputElement} inputElement
+*/
+function isCheckBoxClicked(inputElement) {
+return inputElement.checked === true;
+}
 
-function deleteCheckedToDos() {   // Get the parent UL that contains the to dos 
-    var ul = document.getElementById("toDoList");  
-    // Get all the list items from the to do list 
-    var items = ul.getElementsByTagName("li");  
-    // Iterate over the list 
-    for (var i = 0; i < items.length; ++i) { 
-      //Get all the check box elements from each to-do 
-      var checkbox = items[i].childNodes[0].childNodes[1];  
-      // If the check box is checked 
-      if(checkbox.checked === true) { 
-        // Remove the todo from the list 
-        ul.removeChild(items[i]); 
-      } 
-    }
- }
+/**
+* Create a new ToDo from the text in the input element
+*/
+function createNewToDo() {
+const newToDoText = document.getElementById('newToDoText');
+
+// Guard clause to ensure that there is a value in the input element.
+if (!newToDoText.value) {
+return;
+}
+
+toDos.push({
+title: newToDoText.value,
+complete: false,
+todoId:id,
+});
+
+newToDoText.value = '';
+id=id+1;
+
+renderTheUI();
+}
+
+/**
+* Render the UI
+*/
+function renderTheUI() {
+const toDoList = document.getElementById('toDoList');
+
+toDoList.textContent = '';
+
+toDos.forEach(function(toDo) {
+//created new list and contents for LI
+let newLi = document.createElement('li');
+let span=document.createElement("span");
+let checkbox = document.createElement('input');
+let deleteButton = document.createElement("button");
+let deleteButtonIcon=document.createElement("i");
+
+//add the MDL classes to each created element
+newLi.classList.add("mdl-list__item");
+span.classList.add("mdl-list__item-primary-content")
+deleteButton.classList.add("mdl-button", "mdl-js-button", "mdl-button--fab");
+deleteButtonIcon.classList.add("material-icons");
+
+// set li childNodes values
+checkbox.type = "checkbox";
+span.textContent = toDo.title;
+deleteButtonIcon.textContent = "remove";
+newLi.id=toDo.todoId;
+
+//create eventListener
+deleteButton.addEventListener('click', event=> {
+let buttonElement = event.target;
+deleteCheckedToDos(buttonElement);
+});
+
+// adding all elements to to-do list ul
+deleteButton.appendChild(deleteButtonIcon);
+span.appendChild(checkbox);
+span.appendChild(deleteButton);
+newLi.appendChild(span);
+toDoList.appendChild(newLi);
+});
+}
+
+// Add an event listener to the submit button in the form.
+addToDoForm.addEventListener('submit', event => {
+event.preventDefault();
+createNewToDo();
+});
+
+renderTheUI();
+}
+
 window.onload = function() {
-  onReady();
+onReady();
 };
