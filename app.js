b8590d3707b4106b77b8236d4b95c767fbf513de
Brittany <brittany.foster@yahoo.com>
2019-09-08 17:11:17 -0700

    fixed deleteButton issues

--- app.js
+++ app.js
@@ -1,108 +1,108 @@
 function onReady() {
-let id = 0;
-let toDos = [];
-const addToDoForm = document.getElementById('addToDoForm');
+    let id = 0;
+    let toDos = [];
+    const addToDoForm = document.getElementById('addToDoForm');
 
-/**
-* Delete the list item that the Delete Button is in.
-* @param {HTMLButtonElement} buttonElement
-*/
-function deleteCheckedToDos(buttonElement) {
-let liElement = buttonElement.parentNode.parentNode.parentNode;
-var liId = parseInt(liElement.id);
-toDos = toDos.filter(function(toDo) {
-return !(toDo.todoId === liId
-&& isCheckBoxClicked(buttonElement.parentElement.parentElement.childNodes[1]));
-});
+    /**
+     * Delete the list item that the Delete Button is in.
+     * @param {HTMLButtonElement} buttonElement
+     */
+    function deleteCheckedToDos(li) {
 
-id = id - 1;
-// Once the appropriate elements have been deleted, render the UI
-renderTheUI();
-}
+        let input=li.querySelector("input");
+        var liId = parseInt(li.id);
+        toDos = toDos.filter(function(toDo) {
+            return !(toDo.todoId === liId &&
+                isCheckBoxClicked(input));
+        });
 
-/**
-* Check to see if the input element checkbox is checked.
-* @param {HTMLInputElement} inputElement
-*/
-function isCheckBoxClicked(inputElement) {
-return inputElement.checked === true;
-}
+        id = id - 1;
+        // Once the appropriate elements have been deleted, render the UI
+        renderTheUI();
+    }
 
-/**
-* Create a new ToDo from the text in the input element
-*/
-function createNewToDo() {
-const newToDoText = document.getElementById('newToDoText');
+    /**
+     * Check to see if the input element checkbox is checked.
+     * @param {HTMLInputElement} inputElement
+     */
+    function isCheckBoxClicked(inputElement) {
+        return inputElement.checked === true;
+    }
 
-// Guard clause to ensure that there is a value in the input element.
-if (!newToDoText.value) {
-return;
-}
+    /**
+     * Create a new ToDo from the text in the input element
+     */
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
 
-toDos.push({
-title: newToDoText.value,
-complete: false,
-todoId:id,
-});
+        // Guard clause to ensure that there is a value in the input element.
+        if (!newToDoText.value) {
+            return;
+        }
 
-newToDoText.value = '';
-id=id+1;
+        toDos.push({
+            title: newToDoText.value,
+            complete: false,
+            todoId: id,
+        });
 
-renderTheUI();
-}
+        newToDoText.value = '';
+        id = id + 1;
 
-/**
-* Render the UI
-*/
-function renderTheUI() {
-const toDoList = document.getElementById('toDoList');
+        renderTheUI();
+    }
 
-toDoList.textContent = '';
+    /**
+     * Render the UI
+     */
+    function renderTheUI() {
+        const toDoList = document.getElementById('toDoList');
 
-toDos.forEach(function(toDo) {
-//created new list and contents for LI
-let newLi = document.createElement('li');
-let span=document.createElement("span");
-let checkbox = document.createElement('input');
-let deleteButton = document.createElement("button");
-let deleteButtonIcon=document.createElement("i");
+        toDoList.textContent = '';
 
-//add the MDL classes to each created element
-newLi.classList.add("mdl-list__item");
-span.classList.add("mdl-list__item-primary-content")
-deleteButton.classList.add("mdl-button", "mdl-js-button", "mdl-button--fab");
-deleteButtonIcon.classList.add("material-icons");
+        toDos.forEach(function(toDo) {
+            //created new list and contents for LI
+            let newLi = document.createElement('li');
+            let span = document.createElement("span");
+            let checkbox = document.createElement('input');
+            let deleteButton = document.createElement("button");
+            let deleteButtonIcon = document.createElement("i");
 
-// set li childNodes values
-checkbox.type = "checkbox";
-span.textContent = toDo.title;
-deleteButtonIcon.textContent = "remove";
-newLi.id=toDo.todoId;
+            //add the MDL classes to each created element
+            newLi.classList.add("mdl-list__item");
+            span.classList.add("mdl-list__item-primary-content")
+            deleteButton.classList.add("mdl-button", "mdl-js-button", "mdl-button--fab");
+            deleteButtonIcon.classList.add("material-icons");
 
-//create eventListener
-deleteButton.addEventListener('click', event=> {
-let buttonElement = event.target;
-deleteCheckedToDos(buttonElement);
-});
+            // set li childNodes values
+            checkbox.type = "checkbox";
+            span.textContent = toDo.title;
+            deleteButtonIcon.textContent = "remove";
+            newLi.id = toDo.todoId;
 
-// adding all elements to to-do list ul
-deleteButton.appendChild(deleteButtonIcon);
-span.appendChild(checkbox);
-span.appendChild(deleteButton);
-newLi.appendChild(span);
-toDoList.appendChild(newLi);
-});
-}
+            //create eventListener
+            deleteButton.addEventListener('click', event => {
+                deleteCheckedToDos(newLi);
+            });
 
-// Add an event listener to the submit button in the form.
-addToDoForm.addEventListener('submit', event => {
-event.preventDefault();
-createNewToDo();
-});
+            // adding all elements to to-do list ul
+            deleteButton.appendChild(deleteButtonIcon);
+            span.appendChild(checkbox);
+            span.appendChild(deleteButton);
+            newLi.appendChild(span);
+            toDoList.appendChild(newLi);
+        });
+    }
 
-renderTheUI();
+    // Add an event listener to the submit button in the form.
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+    });
+
+    renderTheUI();
 }
 
 window.onload = function() {
-onReady();
+    onReady();
 };
